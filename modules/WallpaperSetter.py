36e463eeedf47ae9b11e43549b71d0836cd7c57b
Jared J <26422790+ipaqmaster@users.noreply.github.com>
2024-12-02 13:49:10 +1100

    Continued work on the script
    Get mime types with magic module
    
    Add an image loader to reduce repeat processing of the same file
    
    Add updateImageProfile for getting a dominant color profile for the image as hex
    
    Handle an image more efficiently when processing is needed

--- modules/WallpaperSetter.py
+++ modules/WallpaperSetter.py
@@ -4,6 +4,7 @@ from dateutil import parser
 import PIL
 import datetime
 import hashlib
+import magic
 import os
 import re
 
@@ -41,12 +42,39 @@ class WallpaperSetter:
         else:
             return False
   
-    def updateImageResolution(self,filePath):
+
+    def loadImage(self, filePath):
+        self.img       = None
+        self.imgHeight = None
+        self.imgWidth  = None
+
+        # Allow clearing without opening a new image
+        if filePath:
+            try:
+                self.img                      = PIL.Image.open(filePath)
+                self.imgWidth, self.imgHeight = self.img.size
+            except:
+                print("Failed to open file:\t%s" % filePath)
+                return False
+
+            return True
+  
+    def updateImageMime(self, filePath):
+        mime = magic.from_file(filePath, mime=True)
+        if mime:
+            columns = ["mime"]
+            values  = [mime]
+            where   = [('path', '=', filePath)]
+            result  = self.database.query(columns=columns, values=values, mode='update', where=where)
+
+    def updateImageResolution(self, filePath):
+        if not self.img:
+            if not self.loadImage(filePath):
+                return False
+
         try:
-            img           = PIL.Image.open(filePath)
-            width, height = img.size
             columns = ["width", "height"]
-            values  = [width, height]
+            values  = [self.imgWidth, self.imgHeight]
             where   = [('path', '=', filePath)]
             result  = self.database.query(columns=columns, values=values, mode='update', where=where)
             return result
@@ -65,44 +93,103 @@ class WallpaperSetter:
                                          mode='update')
             return result
 
+
+    def updateImageProfile(self, filePath):
+        """Get the dominant color (profile) of an image. Slower without thumbnail"""
+
+        if not self.img:
+            if not self.loadImage(filePath):
+                return False
+
+        # Make a copy of the image and shrink it for processing.
+        thumb = self.img.copy()
+        thumb.thumbnail((100,100))
+
+        width, height = thumb.size
+
+        if thumb.mode != 'RGB':
+            thumb = thumb.convert('RGB')
+    
+        r_total = 0
+        g_total = 0
+        b_total = 0
+    
+        count = 0
+        for x in range(0, width):
+            for y in range(0, height):
+                pixel = thumb.getpixel((x,y))
+                # Ignore a potential alpha channel in the fourth position.
+                r, g, b = pixel[0], pixel[1], pixel[2]
+                r_total += r
+                g_total += g
+                b_total += b
+                count += 1
+    
+        profile = "%02x%02x%02x" % (int(r_total/count), int(g_total/count), int(b_total/count))
+
+        result = self.database.query(columns=["profile"],
+                                      values=[profile],
+                                       where=[('path', '=', filePath)],
+                                        mode='update')
+
+        return result
+
     def getImageRow(self, filePath):
         return(self.database.execFetchoneDict("select * from wallpapers where path = '%s'" % filePath))
-  
+
     def updateDb(self):
-        print("Regenerating the database.")
-        print("Scanning for wallpapers in: %s" % self.directory)
+        print("Refreshing the database.")
+        print("Scanning: %s" % self.directory)
   
         now    = datetime.datetime.now()
         nowStr = str(now)
   
         for root, dirs, files in os.walk(self.directory):
             for filename in files:
+                self.loadImage(None) # Reset
                 filePath = os.path.join(root, filename)
                 if 'disabled' in filePath:
                     continue
                 else:
                     result = self.getImageRow(filePath)
                     if not result: # Add the image path to the database
-                        self.database.query(columns=["path", "added", "seen"], values=[filePath, nowStr, nowStr])
+                        self.database.query(columns=["path", "added", "seen"],
+                                             values=[filePath, nowStr, nowStr])
                         result = self.getImageRow(filePath)
 
                     # Update the last seen value of this file
                     self.database.query(columns=["seen"],
-                                        values=[nowStr],
-                                        where=[('path', '=', filePath)],
-                                        mode='update')
-                      
+                                         values=[nowStr],
+                                          where=[('path', '=', filePath)],
+                                           mode='update')
+
+                    if self.debug:
+                        for key in result.keys():
+                            if result[key] is None:
+                                print(filePath)
+                                break
+
+                    if not result['mime']:
+                        if self.debug:
+                            print("\tGetting mime type...")
+
+                        self.updateImageMime(filePath)
+
                     if not result['width'] or not result['height']:
+                        if self.debug:
+                            print("\tGetting dimensions...")
+
                         self.updateImageResolution(filePath)
 
                     if not result['hash']:
+                        if self.debug:
+                            print("\tHashing...")
+
                         self.updateImageHash(filePath)
   
-  
-                  
-                    #columns = ["path", "seen"]
-                    #values  = [filePath, nowStr]
-                    #self.database.query(columns=columns, values=values)
-  
-  
-  
+                    if not result['profile']:
+                        if self.debug:
+                            print("\tProfiling...")
+
+                        self.updateImageProfile(filePath)
+
